b92eec8b90e5cfe120e96d03788eecf722d65f63
phsharma <prakashharisharma@gmail.com>
2017-11-10 12:55:37 +0530

    1

--- spring-boot/src/main/java/com/example/controller/ExampleController.java
+++ spring-boot/src/main/java/com/example/controller/ExampleController.java
@@ -5,6 +5,13 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
+
+/**
+ * @author phsharma
+ *
+ */
+
 @RestController
 @EnableAutoConfiguration
 public class ExampleController {
