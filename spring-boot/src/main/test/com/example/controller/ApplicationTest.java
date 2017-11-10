715b78c4b5a93809da44a8a7866d7fa016dc3928
phsharma <prakashharisharma@gmail.com>
2017-11-10 15:10:03 +0530

    updated

---
+++ spring-boot/src/main/test/com/example/controller/ApplicationTest.java
@@ -0,0 +1,42 @@
+package com.example.controller;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.example.controller.ExampleController;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = ExampleController.class)
+@AutoConfigureMockMvc
+public class ApplicationTest {
+
+	@Autowired
+    private ExampleController controller;
+
+    @Autowired
+    private MockMvc mockMvc;
+    
+    @Test
+    public void contexLoads() throws Exception {
+        assertThat(controller).isNotNull();
+    }
+
+    @Test
+    public void shouldReturnDefaultMessage() throws Exception {
+        this.mockMvc.perform(get("/")).andDo(print()).andExpect(status().isOk())
+                .andExpect(content().string(containsString("Hello World")));
+    }
+    
+}
